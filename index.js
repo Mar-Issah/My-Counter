fa035171cf2ed88d53b9ecfafe02e7796839576b
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-04-01 12:21:34 -0400

    counter project new

---
+++ index.js
@@ -0,0 +1,38 @@
+//set the initial count
+let count = 0;
+
+
+//call the value and buttons in a variable
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("increase")) {
+            count++
+        } else if (styles.contains("decrease")){
+            count--;
+        }
+        else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green"
+        }
+
+        if (count < 0) {
+            value.style.color = "red"
+        }
+
+        if (count === 0) {
+            value.style.color = "black"
+        }
+        value.textContent = count;
+
+        
+     });
+ });
+
